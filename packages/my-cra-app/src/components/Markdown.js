0f3d1eb802a0dac3ff4ec88f149fb633b61d1858
Te-Chi Liu <liuderchi@gmail.com>
2018-01-23 17:33:28 +0800

    lab(react-markdown): add <Markdown />

---
+++ packages/my-cra-app/src/components/Markdown.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import './Markdown.css'
+
+const input = `
+# This is a header
+## h2
+### h3
+#### h4
+##### h5
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+__bold__
+**bold v2**
+
+_italic_
+*italic v2*
+
+<u>text with underscore</u>
+
+<strike>deleted</strike>
+
+<foo>colored</foo>
+`
+
+const Markdown = () => (
+  <ReactMarkdown
+    className="Markdown"
+    source={input}
+    escapeHtml={false}
+  />
+)
+
+export default Markdown;
